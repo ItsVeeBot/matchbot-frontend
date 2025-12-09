b9124642ee2c7113fd17c8cc480180adbf63ea80
Bryan Hauser <vmkid95@gmail.com>
2025-12-09 17:12:41 -0500

    Debug. Why won't this work?

--- app/socket.ts
+++ app/socket.ts
@@ -3,4 +3,6 @@ import { io } from 'socket.io-client'
 // const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3821'
 const URL = import.meta.env.VITE_SERVER_URL
 
-export const socket = io(URL, {secure:true});
+console.log("Trying to connect to ", URL)
+
+export const socket = io(URL);
