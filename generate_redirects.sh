3c0acaf944b6f7fbdc7cbb7c10926414367e07c9
Bryan Hauser <vmkid95@gmail.com>
2025-12-09 11:57:01 -0500

    Change directory for generating redirects?

--- generate_redirects.sh
+++ generate_redirects.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "/socket.io/* $VITE_SERVER_URL/:splat 200" > public/_redirects
-cat public/_redirects
+echo "/socket.io/* $VITE_SERVER_URL/:splat 200" > _redirects
+cat _redirects
