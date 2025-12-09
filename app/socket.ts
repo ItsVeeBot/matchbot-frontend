ad981e49b15b47a599fb874e3c913da828dfc5e0
Bryan Hauser <vmkid95@gmail.com>
2025-12-09 17:01:47 -0500

    Attempt to use SSL

--- app/socket.ts
+++ app/socket.ts
@@ -3,4 +3,4 @@ import { io } from 'socket.io-client'
 // const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3821'
 const URL = import.meta.env.VITE_SERVER_URL
 
-export const socket = io(URL);
+export const socket = io(URL, {secure:true});
